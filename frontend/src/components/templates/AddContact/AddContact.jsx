e824babf1a7b4db38374cbfb62b2caea3458dc01
anoj17 <anojbudathoki17@gmail.com>
2024-10-21 12:54:52 +0545

    changes

--- frontend/src/components/templates/AddContact/AddContact.jsx
+++ frontend/src/components/templates/AddContact/AddContact.jsx
@@ -129,6 +129,21 @@ const AdminAddSignal = () => {
             handleClick={handleRouteRTMS}
           />
         </StaffSidebar>
+         {/* <div
+            className={`
+              flex justify-between items-center
+              overflow-hidden transition-all ${expanded ? "w-52 ml-3" : "w-0"}
+          `}
+          >
+            <div className="leading-4">
+              <h4 className="font-semibold">Prajwol Subedi</h4>
+              <span className="text-xs text-gray-600">
+                prajwolsubedi@gmail.com
+              </span>
+            </div>
+            <MoreVertical size={20} />
+          </div>
+        </div> */}
       </Box>
       <Box
         sx={{ display: "flex", justifyContent: "space-between", width: "100%" }}
