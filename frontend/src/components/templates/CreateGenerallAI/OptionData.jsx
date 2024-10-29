beae20d9ee4bc4ffc3668d3fadefc6686beb77c6
Ninetel <reusereuse11@gmail.com>
2024-10-29 17:51:20 +0545

    Order staff chat2

---
+++ frontend/src/components/templates/CreateGenerallAI/OptionData.jsx
@@ -0,0 +1,26 @@
+export const LearnAndEarn = [
+    "Technical Analysis Training (Basic)",
+    "Technical Analysis Training (Intermediate)",
+    "Technical Analysis Training (Advanced)",
+    "100+ Rich content training Videos",
+    "Technical Analysis Training (Basic)",
+    "Technical Analysis Training (Advanced)",
+    "Smart Money Concepts",
+    "Fundamental analysis training",
+    "Free ebooks and resources",
+    "Price Rs. (3500 for 3 months, 6000 for 6 months, 10000 for 1 year)"
+  ];
+  
+  export const TradeSupport = [
+    "24/7 Trade Assistance",
+    "Risk Management Guidelines",
+    "Trade Signal Alerts",
+    "Dedicated Support Team",
+    "Real-Time Issue Resolution",
+    "Market Insights and Analytics",
+    "Price Rs. (3000 for 3 months, 5500 for 6 months, 9500 for 1 year)"
+  ];
+  
+
+ 
+ 
